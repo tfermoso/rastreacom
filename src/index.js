11f64d704eeac5a81f8153e52af547df1dd95cc9
tfermoso <tomas.fermoso@gmail.com>
2026-03-02 17:15:23 +0100

    ejs

--- src/index.js
+++ src/index.js
@@ -12,9 +12,12 @@ const app = express();
 // serve frontend static files
 app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(express.json());
+app.set("view engine", "ejs");
 
+// Carpeta de vistas (ajusta según tu estructura)
+app.set("views", path.join(__dirname, "views"));
 mongoose.connect(process.env.MONGODB_URI)
-.then(() => console.log("Mongo conectado"));
+    .then(() => console.log("Mongo conectado"));
 
 app.use("/auth", authRoutes);
 
